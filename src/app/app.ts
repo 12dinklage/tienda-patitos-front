20fb093483713b4096e653ae6406cd5bc6dd4c5e
12dinklage <clau.pachecoypurani@gmail.com>
2026-07-28 02:47:47 -0400

    cambios

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,8 +3,8 @@ import { Pato } from './pato.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
 export class AppComponent {
   pestanaActiva: 'patos' | 'ventas' = 'patos';
